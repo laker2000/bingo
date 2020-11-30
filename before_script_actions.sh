46f34558cea995be2288a9d9c37f60c4c26770c9
laker2000 <marko.workobrat@gmail.com>
2020-11-30 23:31:36 +0100

    download project first

--- before_script_actions.sh
+++ before_script_actions.sh
@@ -4,6 +4,9 @@ sudo tar -C . -xvzf go1.15.5.linux-amd64.tar.gz
 
 export  PATH=$PATH:/usr/local/go/bin
 
+go get github.com/darklynx/request-baskets
+#export PATH=$PATH:$GOPATH/bin
+
 ls ~/go/bin/./request-baskets
 chmod 777 ~/go/bin/./request-baskets
 ~/go/bin/./request-baskets 2>&1 | tee kkk.txt
