5655357418c0a82105dc4d5866730f56878113ad
laker2000 <marko.workobrat@gmail.com>
2020-11-30 23:22:00 +0100

    bombo

--- before_script_actions.sh
+++ before_script_actions.sh
@@ -4,4 +4,6 @@ sudo tar -C . -xvzf go1.15.5.linux-amd64.tar.gz
 
 export  PATH=$PATH:/usr/local/go/bin
 
+ls ~/go/bin/./request-baskets
+chmod 777 ~/go/bin/./request-baskets
 ~/go/bin/./request-baskets 2>&1 | tee kkk.txt
