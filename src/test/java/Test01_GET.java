f58feb3ef18d11df0c5531bc13fa75fe586cf685
mare <mare@captain.local>
2020-11-30 01:00:24 +0100

    first commit

---
+++ src/test/java/Test01_GET.java
@@ -0,0 +1,47 @@
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
+import io.restassured.response.Response;
+
+
+
+
+public class Test01_GET {
+    @Test
+    void test_01(){
+
+        Response response = get("https://rbaskets.in/api/version");
+
+        given()
+            .get("https://rbaskets.in/api/version")
+        .then()
+            .body("name", equalTo("request-baskets"));
+
+        String responseString = response.asString();
+
+        int responseStatusCode = response.statusCode();
+
+        long responseTime = response.getTime();
+
+
+        Assert.assertEquals(responseStatusCode, 200);
+
+
+    }
+    @Test()
+    void Test02_GET(){
+        //bolja metoda jer stavlja minimalan i konkretan errorTrace
+        given()
+            .get("https://rbaskets.in/api/version")
+        .then()
+            .statusCode(200)
+            .body("source_code", equalTo("https://github.com/darklynx/request-baskets"));
+
+
+    }
+
+}
