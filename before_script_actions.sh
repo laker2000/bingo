74570e5e24002a46ed3b24c80be4e9578754712b
laker2000 <marko.workobrat@gmail.com>
2020-12-01 01:02:43 +0100

    anotjher

--- before_script_actions.sh
+++ before_script_actions.sh
@@ -3,11 +3,9 @@ wget -c https://golang.org/dl/go1.15.5.linux-amd64.tar.gz
 sudo tar -C . -xvzf go1.15.5.linux-amd64.tar.gz
 
 export  PATH=$PATH:/usr/local/go/bin
-ls -ltra ~/
-go version
-go get github.com/darklynx/request-baskets
-#export PATH=$PATH:$GOPATH/bin
 
-ls ~/go/bin/./request-baskets
+go get github.com/darklynx/request-baskets
+
 chmod 777 ~/go/bin/./request-baskets
+
 ~/go/bin/./request-baskets 2>&1 | grep token | sed 's/^.*: //' | tee kkk.txt &
