01fd5998548d9d49fa580686012356c0871ccbae
laker2000 <marko.workobrat@gmail.com>
2020-12-02 23:33:51 +0100

    Finish him TestBasketsPostWrongName

--- src/test/java/tests/api/baskets/post/TestBasketsPostWrongName.java
+++ src/test/java/tests/api/baskets/post/TestBasketsPostWrongName.java
@@ -13,12 +13,12 @@ import static org.hamcrest.Matchers.is;
 public class TestBasketsPostWrongName extends TestConfig {
 
     /**
-     * Scenario: Check creation of a basket with with wrong name values in order to provoke Bad Request 400 response
+     * Scenario: Check creation of a basket with wrong name values in order to provoke Bad Request 400 response
      *
      * Test steps:
      *
      * 1. Arrange a basket for creation
-     * 2. Try to create a basket with wrong name parameter
+     * 2. For each provided name try to create a basket with wrong given name parameter
      * 3. Assert that 400 code is received, Assert response message
      *
      * */
@@ -40,7 +40,7 @@ public class TestBasketsPostWrongName extends TestConfig {
             requestSpec = builder.build();
             request = RestAssured.given().spec(requestSpec);
 
-            // 2. Try to create a basket with wrong name parameter
+            // 2. For each provided name try to create a basket with wrong given name parameter
             var postResponse = request.post(basketByName);
 
             // 3. Assert that 400 code is received, Assert response message
@@ -53,12 +53,12 @@ public class TestBasketsPostWrongName extends TestConfig {
 
 
     /**
-     * Scenario: Check creation of a basket with with wrong name values in order to provoke NOT FOUND 404 response
+     * Scenario: Check creation of a basket with wrong name values in order to provoke NOT FOUND 404 response
      *
      * Test steps:
      *
      * 1. Arrange a basket for creation
-     * 2. Try to create a basket with wrong name parameter
+     * 2. For each provided name try to create a basket with wrong given name parameter
      * 3. Assert that 404 code is received, Assert response message
      *
      * */
@@ -68,7 +68,7 @@ public class TestBasketsPostWrongName extends TestConfig {
     public void testBasketsPostWrongNameNotFound(){
         // 1. Arrange a basket for creation
 
-        List<String> wrongNames = Arrays.asList("", "<script>alert('Injected!');</script>");
+        List<String> wrongNames = Arrays.asList("", "<script>alert('Injected!');</script>", "api/stats");
         RequestSpecBuilder builder = new RequestSpecBuilder();
         builder.addHeader("Authorization", serviceToken);
         var requestSpec = builder.build();
@@ -80,12 +80,49 @@ public class TestBasketsPostWrongName extends TestConfig {
             requestSpec = builder.build();
             request = RestAssured.given().spec(requestSpec);
 
-            // 2. Try to create a basket with wrong name parameter
+            // 2. For each provided name try to create a basket with wrong given name parameter
             var postResponse = request.post(basketByName);
 
             // 3. Assert that 404 code is received, Assert response message
             assertThat(postResponse.statusCode(), is(404));
 
+        }
+
+    }
+
+    /**
+     * Scenario: Check creation of a basket with reserved name values in order to provoke Forbidden 403 response
+     *
+     * Test steps:
+     *
+     * 1. Arrange a basket for creation
+     * 2. For each provided name try to create a basket with reserved name ("api", "baskets", "web")
+     * 3. Assert that 403 code is received, Assert response message
+     *
+     * */
+    @Test
+    public void testBasketsPostWrongNameReservedWords(){
+        // 1. Arrange a basket for creation
+        List<String> wrongNames = Arrays.asList("api", "baskets", "web");
+        RequestSpecBuilder builder = new RequestSpecBuilder();
+        builder.addHeader("Authorization", serviceToken);
+        var requestSpec = builder.build();
+        var request = RestAssured.given().spec(requestSpec);
+        String expectedMessage;
+
+        for (String name : wrongNames) {
+
+            builder.addPathParam("name", name);
+            requestSpec = builder.build();
+            request = RestAssured.given().spec(requestSpec);
+
+            // 2. For each provided name try to create a basket with wrong given name parameter
+            var postResponse = request.post(basketByName);
+
+            // 3. Assert that 400 code is received, Assert response message
+            assertThat(postResponse.statusCode(), is(403));
+            expectedMessage = String.format("This basket name conflicts with reserved system path: %s\n", name);
+            assertThat(postResponse.body().asString(), is(expectedMessage));
         }
 
     }
