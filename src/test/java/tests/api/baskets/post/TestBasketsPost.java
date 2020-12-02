6ffa742e18a763e103e491b9a5c47ac6132fb5a5
laker2000 <marko.workobrat@gmail.com>
2020-12-02 23:34:15 +0100

    Done TestBasketsPost

--- src/test/java/tests/api/baskets/post/TestBasketsPost.java
+++ src/test/java/tests/api/baskets/post/TestBasketsPost.java
@@ -2,30 +2,62 @@ package tests.api.baskets.post;
 
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.http.ContentType;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.annotations.Test;
 
 import tests.TestConfig;
 
+import java.util.HashMap;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class TestBasketsPost extends TestConfig {
 
+    /**
+     * Scenario: Check creation of a basket
+     *
+     * Test steps:
+     *
+     * 1. Arrange a basket for creation
+     * 2. Create a given basket
+     * 3. Assert that basket is created
+     *
+     * */
     @Test
-    public void testBasketPost(){
+    public void testBasketsPost(){
+        // 1. Arrange a basket for creation
+        String basketName = RandomStringUtils.randomAlphanumeric(10);
+
         RequestSpecBuilder builder = new RequestSpecBuilder();
-        builder.setBaseUri(RestAssured.baseURI);
-        builder.setContentType(ContentType.JSON);
-        String basketName = "mark3o";
         builder.addPathParam("name", basketName);
         builder.addHeader("Authorization", serviceToken);
+
+        HashMap<String, Object> basketContent = new HashMap<>();
+        basketContent.put("forward_url", "https://nba.com");
+        basketContent.put("proxy_response", true);
+        basketContent.put("insecure_tls", true);
+        basketContent.put("expand_path", true);
+        basketContent.put("capacity", 321);
+
+        // 2. Create a given basket
         var requestSpec = builder.build();
         var request = RestAssured.given().spec(requestSpec);
+        request.body(basketContent);
 
         var postResponse = request.post(basketByName);
-        System.out.println();
+        assertThat(postResponse.statusCode(), is(201));
+
+        // 3. Assert that basket is created
+        postResponse = request.get(basketByName);
+        assertThat(postResponse.statusCode(), is(200));
+        assertThat(postResponse.getBody().jsonPath().get("forward_url"), is("https://nba.com"));
+        assertThat(postResponse.getBody().jsonPath().get("proxy_response"), is(true));
+        assertThat(postResponse.getBody().jsonPath().get("insecure_tls"), is(true));
+        assertThat(postResponse.getBody().jsonPath().get("expand_path"), is(true));
+        assertThat(postResponse.getBody().jsonPath().get("capacity"), is(321));
 
     }
 
-    //todo: test kada dva put kreiras basket sa istim imenom, proeris 409 kod, ali i proveri
-    //todo za sv slucaj da li se na UI prikazuje duplikat
 
 }
